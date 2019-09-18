5099ab88651a772238f9326557f7602823be8725
Avinoam Ruach <ruachavinoam@gmail.com>
2019-09-18 10:36:54 -0700

    update index

---
+++ algorithms/daysLived/days.js
@@ -0,0 +1,18 @@
+var name = prompt("What is your name?");
+var birthday = parseInt(prompt("What day did you born on?"));
+var month = parseInt(prompt("What month is your birthday on?"));
+var year = parseInt(prompt("What year did you born on?"));
+var day = 0;
+
+
+function days(birthday, month, year){
+
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    switch (daysLived){
+
+        case '1':
+            document.getElementById("output").innerHTML = day + months(0);
+    }
+
+
+}
