a5593a1bb02a5ef409d07927f1f3a6fec4e6cfd4
Avinoam Ruach <ruachavinoam@gmail.com>
2019-10-03 21:28:36 -0700

    game1 update

--- FinalProject/AviJS/game1.js
+++ FinalProject/AviJS/game1.js
@@ -1,24 +1,32 @@
 
 function checkPrime(){
     var inputNumber = document.getElementById("num").value;
-    var flag = true;
-    if (inputNumber == 0){
-        flag = false;
+    if (isNaN(inputNumber))
+    {
+        alert("Please input numbers");
+        return false;
     }
-    if (inputNumber == 1){
-        flag = true;
-    }
-    for(var i = 2; i < inputNumber; i++) {
-        if(inputNumber % i === 0) {
+    else {
+
+        var flag = true;
+        if (inputNumber == 0){
             flag = false;
         }
-        
-    }if (flag == true){
-        document.getElementById("result").innerHTML ="The number " + inputNumber + " is a prime number.";
-    }if (flag == false){
-        document.getElementById("result").innerHTML = "The number " + inputNumber + " is not a prime number.";
+        if (inputNumber == 1){
+            flag = true;
+        }
+        for(var i = 2; i < inputNumber; i++) {
+            if(inputNumber % i === 0) {
+                flag = false;
+            }
+            
+        }if (flag == true){
+            document.getElementById("result").innerHTML ="The number " + inputNumber + " is a prime number.";
+        }if (flag == false){
+            document.getElementById("result").innerHTML = "The number " + inputNumber + " is not a prime number.";
+        }
+        // console.log(flag)
+        return flag;
     }
-    // console.log(flag)
-    return flag;
 }
 
---
+++ java/helloWorld.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void Main(string[] args) {
+
+        int age = 23;
+        System.out.printin(age);
+    }
+}
